3765d3605edb94a21a8a3c088ec17b57e1e315d6
afeiship <1290657123@qq.com>
2019-08-12 15:40:51 +0800

    add: pre

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(inTarget) {
-  if (typeof inTarget.prototype.render === "undefined") {
+  if (typeof inTarget.prototype.render === 'undefined') {
     inTarget.prototype.render = function() {
       return null;
     };
